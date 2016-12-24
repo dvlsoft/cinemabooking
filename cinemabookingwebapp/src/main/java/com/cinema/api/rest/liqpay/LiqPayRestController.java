5866ccb2fd6ecca1baf360ada5570d6fb8d8863b
Devastator <alexdubitandum@gmail.com>
2016-12-24 18:21:15 +0200

    Add LiqPay controller

--- cinemabookingwebapp/src/main/java/com/cinema/api/rest/liqpay/LiqPayRestController.java
+++ cinemabookingwebapp/src/main/java/com/cinema/api/rest/liqpay/LiqPayRestController.java
@@ -1,5 +1,7 @@
 package com.cinema.api.rest.liqpay;
 
+import com.cinema.api.rest.liqpay.liqPayApi.LiqPayService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,25 +17,16 @@ import java.util.Map;
 @RequestMapping("/api/rest/liqpay")
 public class LiqPayRestController {
 
+    @Autowired
+    private LiqPayService liqPayService;
 
 
+    @RequestMapping(value = "/account/getLiqPayParam", method = RequestMethod.POST)
+    @ResponseBody
+    public String[] getLiqPayParam(@RequestParam("amount") String email, @RequestParam("amount") Integer amount, @RequestParam("amount") String seatId) {
 
-//
-//    @RequestMapping(value = "/account/getLiqPayParam", method = RequestMethod.POST)
-//    @ResponseBody
-//    public String[] getLiqPayParam(@RequestParam("amount") Long amount) {
-//
-//
-//        if (loggedUserId != null) {
-//            Map<String, String> result = session.liqPayGenerateParamForHtmlForm(loggedUserId, amount);
-//            return new String[]{result.get("data"), result.get("signature")};
-//        } else {
-//            return new String[]{};
-//        }
-//
-//    }
-
-
-
+        Map<String, String> result = liqPayService.liqPayGenerateParamForHtmlForm(email, amount, seatId);
+        return new String[]{result.get("data"), result.get("signature")};
+    }
 
 }
