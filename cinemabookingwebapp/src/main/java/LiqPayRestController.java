fd109e31b717f334bdd36cc042072767581a3786
Devastator <alexdubitandum@gmail.com>
2016-12-24 18:02:51 +0200

    add spring mvc to pom

---
+++ cinemabookingwebapp/src/main/java/LiqPayRestController.java
@@ -0,0 +1,10 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Kobylyatskyy Alexander
+ */
+@Controller
+@RequestMapping("/api/rest/transportService")
+public class LiqPayRestController {
+}
--- model/src/main/java/com/cinema/api/rest/LiqPayRestController.java
+++
@@ -1,7 +0,0 @@
-package com.cinema.api.rest;
-
-/**
- * @author Kobylyatskyy Alexander
- */
-public class LiqPayRestController {
-}
